20a9eb46608e5307386a4ad7390687083d14fe2e
gitachristyp <gitacpurba@gmail.com>
2024-01-31 17:53:52 +0700

    Second Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,7 @@ public class Main {
         //Print dengan new line
         System.out.println("I'm Gita");
         System.out.print("without space\n");
+        System.out.println("lallaa");
 
         boolean flag;
         for (int i = 1; i <= 5; i++) {
