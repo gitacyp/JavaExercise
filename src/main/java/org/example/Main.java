86c932eb5749a43a25d5511e1c9b3f6959c81f90
gitachristyp <gitacpurba@gmail.com>
2024-01-31 17:39:52 +0700

    First Commit Java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Print
+
+        System.out.printf("Hello and welcome!");
+        //Print dengan new line
+        System.out.println("I'm Gita");
+        System.out.print("without space\n");
+
+        boolean flag;
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
